a541f742f67ca1cdcf7f5611fd7576ef223deef2
macdougt <macdougt71@hotmail.com>
2014-05-10 22:31:22 -0400

    Added default localhost URL for the servlet

--- JavaService/src/com/ibm/javaPkg/servlet/SampleServlet.java
+++ JavaService/src/com/ibm/javaPkg/servlet/SampleServlet.java
@@ -5,24 +5,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/** 
+/**
+ * Start at: http://localhost:8080/JavaService/v1/subPath
  */
 @Path("/v1")
 public class SampleServlet {
-		
+
 	@GET
 	@Path("/subPath")
-    @Produces(MediaType.TEXT_PLAIN)     // nature of MIME type  
-    public String simpleStringResponse() {  
-         return "hello from the text response of Hello";  
-    }  
-    @GET  
+    @Produces(MediaType.TEXT_PLAIN)     // nature of MIME type
+    public String simpleStringResponse() {
+         return "hello from the text response of Hello";
+    }
+    @GET
 	@Path("/subPath")
-    @Produces(MediaType.TEXT_HTML) // nature of MIME type  
-    public String simpleHTMLResponse() {  
-         return "<html> <title> Simple RESTful Hello</title> "  
-                   + "<body><h2>Hello from the html response of class Hello :)</h2></body></html>";  
-    } 
+    @Produces(MediaType.TEXT_HTML) // nature of MIME type
+    public String simpleHTMLResponse() {
+         return "<html> <title> Simple RESTful Hello</title> "
+                   + "<body><h2>Hello from the html response of class Hello :)</h2></body></html>";
+    }
 
-	
+
 }
